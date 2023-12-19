37dfce2f47a7160c3ea6db821ec11fe00cc58e09
laismicroni <laismicroni@gmail.com>
2023-12-19 12:04:42 -0300

    html and js tested

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,49 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let words = text.split(' ');
+    return words.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let characters = text.replace(/[^a-zA-Z0-9]/g, '');
+    return characters.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let words = text.split(' ');
+    let sum = 0;
+    for (let i = 0; i < words.length; i++) {
+      sum += words[i].length;
+    }
+    return sum / words.length;
+    
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let numbers = text.split('');
+    let count = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] >= '0' && numbers[i] <= '9') {
+        count++;
+      }
+    }
+    return count;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let numbers = text.split('');
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] >= '0' && numbers[i] <= '9') {
+        sum += parseInt(numbers[i]);
+      }
+    }
+    return sum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`.
+
+document.querySelector('textarea').addEventListener('input', () => {
+    const text = document.querySelector('textarea').value;
+    const wordCount = analyzer.getWordCount(text);
+    document.querySelector('.wordCount').textContent = `Contagem de palavras: ${wordCount}`;
+    const characterCount = analyzer.getCharacterCount(text);
+    document.querySelector('.characterCount').textContent = `Contagem de caracteres: ${characterCount}`;
+    const characterNoSpacesCount = analyzer.getCharacterCountExcludingSpaces(text);
+    document.querySelector('.characterNoSpacesCount').textContent = `Caracteres sem espaços: ${characterNoSpacesCount}`;
+    const numberCount = analyzer.getNumberCount(text);
+    document.querySelector('.numberCount').textContent = `Números: ${numberCount}`;
+    const numberSum = analyzer.getNumberSum(text);
+    document.querySelector('.numberSum').textContent = `Soma de números: ${numberSum}`;
+    const wordLengthAverage = analyzer.getAverageWordLength(text);
+    document.querySelector('.wordLengthAverage').textContent = `Comprimento médio das palavras: ${wordLengthAverage}`;
+    
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('reset-button');
+    button.addEventListener('click', () => {
+        document.querySelector('textarea').value = '';
+        document.querySelector('.wordCount').textContent = 'Contagem de palavras: ';
+        document.querySelector('.characterCount').textContent = 'Contagem de caracteres: ';
+        document.querySelector('.characterNoSpacesCount').textContent = 'Caracteres sem espaços: ';
+        document.querySelector('.numberCount').textContent = 'Números: ';
+        document.querySelector('.numberSum').textContent = 'Soma de números: ';
+        document.querySelector('.wordLengthAverage').textContent = 'Comprimento médio das palavras: ';
+
+    })
+})
+    
